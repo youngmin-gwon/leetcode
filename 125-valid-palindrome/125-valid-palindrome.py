6d0b17930a90c56bdfb64c83340f8ea0eeb5e3eb
Youngmin Kwon <kwonym3094@gmail.com>
2022-09-19 20:52:01 +0900

    Time: 92 ms (34.45%), Space: 19.3 MB (6.07%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,7 +1,13 @@
+import collections
+
 class Solution:
     def isPalindrome(self, s: str)-> str:
-        words = list(re.sub(r"[^\w]*[_]*","", s).lower())
-        while len(words)>1:
-            if (words.pop()!=words.pop(0)):
+        strs=collections.deque()
+        for char in s:
+            if (char.isalnum()):
+                strs.append(char.lower())
+
+        while len(strs)>1:
+            if (strs.pop()!=strs.popleft()):
                 return False
         return True
