5fa9c644ff3a20bad56e461741e8e535aa3deb3e
Youngmin Kwon <kwonym3094@gmail.com>
2022-09-26 23:15:48 +0900

    Time: 2389 ms (5.00%), Space: 22.6 MB (53.21%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,17 @@
+import sys
+
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        
+        profit=0
+        min_price=sys.maxsize
+        
+        for price in prices:
+            min_price=min(min_price,price)
+            profit=max(profit,price-min_price)
+            
+        return profit
