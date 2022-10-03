486ef19a87ac397cf5cca7016d659844306accd3
Youngmin Kwon <kwonym3094@gmail.com>
2022-10-03 11:43:03 +0900

    Time: 155 ms (17.57%), Space: 14 MB (9.90%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -4,36 +4,22 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def reverse_list(self, head:ListNode)->ListNode:
-        node,prev=head,None
-
-        while node:
-            next,node.next=node.next,prev
-            prev,node=node,next
-        return prev
-
-    def to_list(self,node:ListNode)->ListNode:
-        results=[]
-        while node:
-            results.append(node.val)
-            node=node.next
-        return results
-
-    def to_reversed_linked_list(self, result:list)->ListNode:
-        prev:ListNode=None
-        for r in result:
-            node=ListNode(r)
-            node.next=prev
-            prev=node
-        return node
-
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        root=head=ListNode(0)
         
-        a=self.to_list(self.reverse_list(l1))
-        b=self.to_list(self.reverse_list(l2))
-        
-        result_str=int("".join(str(e) for e in a)) + int("".join(str(e) for e in b))
-        
-        return self.to_reversed_linked_list(str(result_str))
-                
+        carry=0
+        while l1 or l2 or carry:
+            sum=0
             
+            if l1:
+                sum+=l1.val
+                l1=l1.next
+            if l2:
+                sum+=l2.val
+                l2=l2.next
+            
+            carry,val=divmod(sum+carry,10)
+            head.next=ListNode(val)
+            head=head.next
+        
+        return root.next
