56a11c84d5a3d80cd7852239b5c66478ddc64dff
Youngmin Gwon <kwonym3094@gmail.com>
2023-08-01 00:11:58 +0900

    Time: 291 ms (100%), Space: 151.1 MB (96.1%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.dart
@@ -0,0 +1,16 @@
+class Solution {
+  List<List<String>> groupAnagrams(List<String> strs) {
+    final Map<String, List<String>> maps = {};
+    for (final str in strs) {
+      final keyChunk = str.split('')..sort((a, b) => a.compareTo(b));
+      final key = keyChunk.join('');
+      if (maps[key] == null) {
+        maps[key] = [str];
+      } else {
+        maps[key]!.add(str);
+      }
+    }
+
+    return maps.values.toList();
+  }
+}
