b2a27437566811fe96025e1a4fcb38fabbb5f01b
Youngmin Gwon <kwonym3094@gmail.com>
2023-08-04 21:39:15 +0900

    Time: 199 ms (0.01%), Space: 16 MB (85.71%) - LeetHub

---
+++ 42-trapping-rain-water/42-trapping-rain-water.dart
@@ -0,0 +1,28 @@
+import 'dart:math';
+
+class Solution {
+  int trap(List<int> height) {
+    if (height.isEmpty) return 0;
+
+    int volume = 0;
+    int left = 0;
+    int right = height.length - 1;
+    int leftMax = height[left];
+    int rightMax = height[right];
+
+    while (left < right) {
+      leftMax = max(height[left], leftMax);
+      rightMax = max(height[right], rightMax);
+
+      if (leftMax <= rightMax) {
+        volume += leftMax - height[left];
+        left += 1;
+      } else {
+        volume += rightMax - height[right];
+        right -= 1;
+      }
+    }
+
+    return volume;
+  }
+}
