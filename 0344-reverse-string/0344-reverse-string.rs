1b0ba9aabe7445e3c9e95d3e03cd195774013dcb
Youngmin Gwon <kwonym3094@gmail.com>
2023-08-21 23:34:28 +0900

    Time: 10 ms (91.47%), Space: 5.6 MB (21.33%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.rs
+++ 0344-reverse-string/0344-reverse-string.rs
@@ -1,5 +1,21 @@
 impl Solution {
     pub fn reverse_string(s: &mut Vec<char>) {
-        s.reverse();
+        if s.len() <= 1 {
+            return;
+        }
+
+        let mut left = 0;
+        let mut right = s.len() - 1;
+
+        while left <= right {
+            let tmp = s[right];
+            s[right] = s[left];
+            s[left] = tmp;
+
+
+            left += 1;
+            right -= 1;
+        }
+    
     }
 }
